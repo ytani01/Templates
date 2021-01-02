a57c0b3c32df8277535fb17699841bffd761a30b
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-01-03 07:01:27 +0900

    [fix] my_logger.py

--- my_logger.py
+++ my_logger.py
@@ -7,12 +7,13 @@ my_logger.py
 __author__ = 'Yoichi Tanibayashi'
 __date__ = '2021'
 
+import inspect
 from logging import getLogger, StreamHandler, Formatter
 from logging import DEBUG, INFO
 # from logging import NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 FMT_HDR = '%(asctime)s %(levelname)s '
-FMT_LOC = '%(filename)s.%(name)s.%(funcName)s:%(lineno)d> '
+FMT_LOC = '%(name)s.%(funcName)s:%(lineno)d> '
 HANDLER_FMT = Formatter(FMT_HDR + FMT_LOC + '%(message)s',
                         datefmt='%H:%M:%S')
 
@@ -20,11 +21,12 @@ CONSOLE_HANDLER = StreamHandler()
 CONSOLE_HANDLER.setFormatter(HANDLER_FMT)
 CONSOLE_HANDLER.setLevel(DEBUG)
 
-
 def get_logger(name, dbg=False):
     """
     get logger
     """
+    filename = inspect.stack()[1].filename.split('/')[-1]
+    name = filename + '.' + name
     logger = getLogger(name)
     logger.propagate = False
     logger.addHandler(CONSOLE_HANDLER)
